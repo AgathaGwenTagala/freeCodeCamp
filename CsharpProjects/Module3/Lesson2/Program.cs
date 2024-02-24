061108b15fe3fadc32d226285d3f6194ac1eb620
Gwen <agathagwen06@gmail.com>
2024-02-24 19:32:56 +0800

    Added "Control variable scope and logic using code blocks in C#"

--- CsharpProjects/Module3/Lesson2/Program.cs
+++ CsharpProjects/Module3/Lesson2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Control variable scope and logic using code blocks in C#
+bool flag = true;
+int value = 0;
+
+if (flag)
+{
+    Console.WriteLine($"Inside the code block: {value}");
+}
+
+value = 10;
+Console.WriteLine($"Outside the code block: {value}");
