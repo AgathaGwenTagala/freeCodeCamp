6022e12809d70f0dbb7fcb154dd86cf4a3bc9297
Gwen <agathagwen06@gmail.com>
2024-02-24 18:52:57 +0800

    Added "Inequality operator versus logical negation"

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -180,6 +180,17 @@ Console.WriteLine(pangram.Contains("cow"));
 */
 
 // Use the Logical Negation operator
-string pangram = "The quick brown fox jumps over the lazy dog.";
+/* string pangram = "The quick brown fox jumps over the lazy dog.";
 Console.WriteLine(!pangram.Contains("fox"));
 Console.WriteLine(!pangram.Contains("cow"));
+*/
+
+// Inequality operator versus logical negation
+/* int a = 7;
+int b = 6;
+Console.WriteLine(a != b); // output: True
+string s1 = "Hello";
+string s2 = "Hello";
+Console.WriteLine(s1 != s2); // output: False
+*/
+
--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d7c41b596730920f5b71bebc4323e631f1af204")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22ee8dcaf8795c9a7130a7c975e66f313383eaf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
